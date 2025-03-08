ae21e5876bb0774f856cdb7dd74344ec79428181
Gabriel de Fombelle <gabriel.de.fombelle@gmail.com>
2025-03-08 18:55:01 +0100

    first version to share 1.1.7

--- pytune_configuration/__init__.py
+++ pytune_configuration/__init__.py
@@ -4,12 +4,6 @@ from pytune_configuration.redis_config import *
 from pytune_configuration.config_service import Config
 from pytune_configuration.redis_listener import RedisListener
 from pytune_configuration.config_change_handler import ConfigChangeHandler
-from pytune_logger.logger import get_logger
-
-# Logger pour le service de configuration
-logger_config = get_logger(name="config_service", index="pytune_configuration")
-logger_config.sync_log_info("Logger for Config initialized")
-
 
 # Exporte explicitement les objets ou fonctions visibles pour les utilisateurs
 __all__ = [
