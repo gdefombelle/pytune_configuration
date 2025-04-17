b46dc65a1facbc82e8215c96977dfcc526497539
Gabriel de Fombelle <gabriel.de.fombelle@gmail.com>
2025-04-17 18:16:23 +0200

    new simple configuration version

--- pytune_configuration/__init__.py
+++ pytune_configuration/__init__.py
@@ -1,17 +1,4 @@
 import asyncio
-from pytune_configuration.postgres_service import DATABASE_CONFIG, PostgresService
 from pytune_configuration.redis_config import *
-from pytune_configuration.config_service import Config
-from pytune_configuration.redis_listener import RedisListener
-from pytune_configuration.config_change_handler import ConfigChangeHandler
 
 # Exporte explicitement les objets ou fonctions visibles pour les utilisateurs
-__all__ = [
-    "DATABASE_CONFIG",
-    "PostgresService",
-    "Config",
-    "RedisListener",
-    "ConfigChangeHandler",
-    "initialize_config",
-    "get_config_instance",
-]
