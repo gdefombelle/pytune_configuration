7392771e9d6d2f850bf9d6be3c1af7385bb6413b
Gabriel de Fombelle <gabriel.de.fombelle@gmail.com>
2025-04-17 21:53:36 +0200

    new simple configuration version

--- pytune_configuration/__init__.py
+++ pytune_configuration/__init__.py
@@ -1,4 +1,8 @@
 import asyncio
 from pytune_configuration.redis_config import *
-
+from pytune_configuration.sync_config_singleton import config, SimpleConfig
 # Exporte explicitement les objets ou fonctions visibles pour les utilisateurs
+__all__ = [
+    "config",
+    "SimpleConfig",
+]
